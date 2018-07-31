2743d3809badab7be10843f709eb76f95e24991e
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-07-31 11:15:49 +0530

    Update Sum_K_Int.java

--- Sum_K_Int.java
+++ Sum_K_Int.java
@@ -1,20 +1,17 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class Sum_K_Int
+class Pos_Neg
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 		int k=s.nextInt();
-		int sum=0;
 		int a[]=new int[n];
 		for(int i=0;i<n;i++)
-		{
-			a[i]=i+1;
-			
-		}
+		a[i]=s.nextInt();
+		int sum=0;
 		for(int i=0;i<k;i++)
 		{
 			sum+=a[i];
